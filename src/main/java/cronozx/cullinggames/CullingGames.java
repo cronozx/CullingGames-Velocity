ab62d8dc6f61e71734d982fc4cdf26e66606a2b7
cronozx <cronozx00@gmail.com>
2025-03-31 21:41:43 -0400

    Added functionality to handle timeouts and canceled games also changed the player leave from the bukkit side to the velocity side so that players leaving is handled better

--- src/main/java/cronozx/cullinggames/CullingGames.java
+++ src/main/java/cronozx/cullinggames/CullingGames.java
@@ -1,6 +1,7 @@
 package cronozx.cullinggames;
 
 import com.google.inject.Inject;
+import com.velocitypowered.api.event.connection.DisconnectEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
@@ -18,8 +19,10 @@ import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 import redis.clients.jedis.JedisPubSub;
 
+import java.net.InetSocketAddress;
 import java.time.LocalTime;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @Plugin(id = "cullinggames", name = "cullinggames", version = BuildConstants.VERSION, authors = {"cronozx"})
@@ -28,6 +31,7 @@ public class CullingGames {
     public static final MinecraftChannelIdentifier IDENTIFIER = MinecraftChannelIdentifier.from("cullinggames:main");
     private final ProxyServer server;
     private final JedisPool jedisPool;
+    private boolean queueOpen = false;
 
     @Inject
     public CullingGames(ProxyServer server, Logger logger) {
@@ -61,30 +65,66 @@ public class CullingGames {
                             if (player.isPresent() && targetServer.isPresent()) {
                                 player.get().createConnectionRequest(targetServer.get()).fireAndForget();
                             }
+                        } else if (message.startsWith("queue:")) {
+                            String playerUUID = message.split(":")[1];
+                            if (queueOpen) {
+                                queuePlayer(playerUUID);
+                            } else {
+                                Optional<Player> optionalPlayer = server.getPlayer(UUID.fromString(playerUUID));
+                                if (optionalPlayer.isPresent()) {
+                                    optionalPlayer.get().sendMessage(Component.newline().content("Culling Games >> The queue is not currently open").color(TextColor.color(255, 0, 0)));
+                                } else {
+                                    logger.warn("Attempted to send message to offline player: {}", playerUUID);
+                                }
+                            }
+                        } else if (message.equals("forceStart")) {
+                            queueOpen = true;
+                            server.getAllServers().forEach(registeredServer -> registeredServer.sendMessage(Component.newline().content("Culling Games >> Culling Games event starting now do /queue to join.").color(TextColor.color(255, 0, 0))));
+                            server.getScheduler().buildTask(CullingGames.this, () -> {
+                                logger.info("Force Start executed");
+                                queueOpen = false;
+                                String forceChannel = "cullinggames:bukkit";
+                                String forceStartMsg = "start";
+                                sendMessage(forceChannel, forceStartMsg);
+                            }).delay(1, TimeUnit.MINUTES).schedule();
+                        } else if (message.startsWith("gameCanceled:")) {
+                            String playerUUID = message.split(":")[1];
+                            Player player = server.getPlayer(UUID.fromString(playerUUID)).get();
+                            InetSocketAddress hubSocketAddress = server.getServer("hub").get().getServerInfo().getAddress();
+
+                            player.transferToHost(hubSocketAddress);
+
+                            player.sendMessage(Component.newline().content("Culling Games >> Not enough players to start."));
+                        } else if (message.startsWith("timeout:")) {
+                            String playerUUID = message.split(":")[1];
+                            Player player = server.getPlayer(UUID.fromString(playerUUID)).get();
+
+                            player.disconnect(Component.newline().content("Timed out"));
                         }
                     }
                 }, "cullinggames:velocity");
             } catch (Exception e) {
-                logger.error("Error subscribing to Redis channel: " + e.getMessage());
+                logger.error("Error subscribing to Redis channel: {}", e.getMessage());
             }
         }).start();
 
-        new Thread(() -> {
-          if (LocalTime.now().getMinute() == 0 && LocalTime.now().getSecond() == 0 && LocalTime.now().getHour() % 3 == 0) {
-              for (RegisteredServer registeredServer: server.getAllServers()) {
-                  registeredServer.sendMessage(Component.newline().content("Culling Games event starting now do /queue to join.").color(TextColor.color(255, 0, 0)));
-                  if (registeredServer.getServerInfo().getName().equals("CullingGames")) {
-                      server.getScheduler().buildTask(this, () -> {
-                        String channel = "cullinggames:bukkit";
-                        String startMsg = "cullinggames:start";
-                        sendMessage(channel, startMsg);
-                      }).delay(5, TimeUnit.MINUTES).schedule();
-                  }
-
-                  logger.info("Message sent");
-              }
-          }
-        });
+        new Thread(() -> server.getScheduler().buildTask(CullingGames.this, () -> {
+            if (LocalTime.now().getMinute() == 0 && LocalTime.now().getSecond() == 0 && LocalTime.now().getHour() % 3 == 0) {
+                clearQueue();
+                this.queueOpen = true;
+                for (RegisteredServer registeredServer: server.getAllServers()) {
+                    registeredServer.sendMessage(Component.newline().content("Culling Games >> Culling Games event starting now do /queue to join.").color(TextColor.color(255, 0, 0)));
+                    if (registeredServer.getServerInfo().getName().equals("CullingGames")) {
+                        server.getScheduler().buildTask(this, () -> {
+                            this.queueOpen = false;
+                            String channel = "cullinggames:bukkit";
+                            String startMsg = "start";
+                            sendMessage(channel, startMsg);
+                        }).delay(5, TimeUnit.MINUTES).schedule();
+                    }
+                }
+            }
+        }).repeat(1, TimeUnit.SECONDS).schedule()).start();
     }
 
     private void sendMessage(String channel, String message) {
@@ -95,11 +135,68 @@ public class CullingGames {
 
     @Subscribe
     public void onProxyDisable(ProxyShutdownEvent event) {
+        clearQueue();
         jedisPool.close();
     }
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
         server.getChannelRegistrar().register(IDENTIFIER);
+    }
+
+    @Subscribe
+    public void onPlayerLeave(DisconnectEvent event) {
+        Player player = event.getPlayer();
+
+        if (playerInQueue(player)) {
+            removePlayerFromQueue(player);
+        }
+
+        if (isPlayerInGames(player)) {
+            removePlayerFromGames(player);
+        }
+    }
+
+    public void clearQueue() {
+        try (Jedis jedis = jedisPool.getResource()) {
+            String key = "playerQueue";
+            String keyType = jedis.type(key);
+
+            if ("list".equals(keyType)) {
+                jedis.del(key);
+            } else {
+                System.err.println("Expected 'playerQueue' to be a string but found: " + keyType);
+            }
+        }
+    }
+
+    public void queuePlayer(String playerUUID) {
+        try (Jedis jedis = jedisPool.getResource()) {
+            jedis.rpush("playerQueue", playerUUID);
+        }
+    }
+
+    public boolean playerInQueue(Player player) {
+        try (Jedis jedis = jedisPool.getResource()) {
+            return jedis.lrange("playerQueue", 0, -1).contains(player.getUniqueId().toString());
+        }
+    }
+
+    public void removePlayerFromQueue(Player player) {
+        try (Jedis jedis = jedisPool.getResource()) {
+            jedis.lrem("playerQueue", 0, player.getUniqueId().toString());
+        }
+    }
+
+    public boolean isPlayerInGames(Player player) {
+        try (Jedis jedis = jedisPool.getResource()) {
+            return jedis.hexists("playerPoints", player.getUniqueId().toString());
+        }
+    }
+
+    public void removePlayerFromGames(Player player) {
+        try (Jedis jedis = jedisPool.getResource()) {
+            jedis.hdel("playerPoints", player.getUniqueId().toString());
+        }
     }
 }
